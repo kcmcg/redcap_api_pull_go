81a37f34f191a1f5348f17dd090803f1805133b1
Kyle McGuffin <kmcguffi@gmail.com>
2024-03-24 14:40:36 -0400

    Break up into multiple functions to make easier to follow/re-use

--- redcap_connect.go
+++ redcap_connect.go
@@ -9,45 +9,23 @@ import (
 	"bytes"
 )
 
-func main() {
-	jsonFile, err := os.Open("../credentials/REDCAP_ZIP_IMPORT_API.json")
-	if err != nil {
-		fmt.Println(err)
-		return
+func sendREDCapRequest(params map[string]string) (map[string]interface{},error) {
+	requestBody := ""
+
+	for param,value := range params {
+		if requestBody != "" {
+			requestBody += "&"
+		}
+		requestBody += param + "=" + value
 	}
-	fmt.Println("successfully Opened json file")
-
-	var token string
-
-	byteValue, err := ioutil.ReadAll(jsonFile)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var result map[string]string
-	json.Unmarshal([]byte(byteValue), &result)
-	token = result["api_token"]
-
-	defer jsonFile.Close()
-
-	fmt.Println(token)
-
 	requestUrl := "https://redcap.vanderbilt.edu/api/"
 
-	jsonBody := []byte(`format=json&content=record&token=` + token + `&returnFormat=json`)
-	bodyReader := bytes.NewReader(jsonBody)
-//	body := make(map[string]string,4)
-//
-//	body["returnFormat"] = "json"
-//	body["action"] = "records"
-//	body["api_token"] = token
-//
+	bodyReader := bytes.NewReader([]byte(requestBody))
+
 	resp, err := http.Post(requestUrl, "application/x-www-form-urlencoded", bodyReader)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil,err
 	}
 
 	fmt.Println("Received a response!")
@@ -62,5 +40,54 @@ func main() {
 
 	json.Unmarshal([]byte(body), &responseString)
 
+	return responseString,nil
+}
+
+func fetchApiToken() (string,error) {
+	jsonFile, err := os.Open("../credentials/REDCAP_ZIP_IMPORT_API.json")
+	if err != nil {
+		fmt.Println(err)
+		return "",err
+	}
+	fmt.Println("successfully Opened json file")
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		fmt.Println(err)
+		return "",err
+	}
+
+	var result map[string]string
+	json.Unmarshal([]byte(byteValue), &result)
+	token := result["api_token"]
+
+	defer jsonFile.Close()
+
+	fmt.Println(token)
+
+	return token,nil
+}
+
+func main() {
+	token, err := fetchApiToken()
+	if err != nil {
+		return
+	}
+
+	requestParams := make(map[string]string)
+
+	requestParams["format"] = "json"
+	requestParams["content"] = "record"
+	requestParams["token"] = token
+	requestParams["returnFormat"] = "json"
+
+//	body := make(map[string]string,4)
+//
+//	body["returnFormat"] = "json"
+//	body["action"] = "records"
+//	body["api_token"] = token
+//
+	responseString,err := sendREDCapRequest(requestParams)
 	fmt.Println(responseString)
 }
